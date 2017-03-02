32166aab8d7370c11bcfa36666dca3986a8bf552
currymich <currymich@gmail.com>
2017-03-01 20:56:52 -0800

    movies and users controllers and models finished

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,40 @@
+class MoviesController < ApplicationController
+  def index
+    render json: {movies: Movie.all}
+  end
+
+  def show
+    render json: {movie: Movie.find(params[:id])}
+  end
+
+  def create
+    movie = Movie.new(movie_params)
+
+    if movie.save
+      render json: {status: 201, movie: movie}
+    else
+      render json: {status: 422}
+    end
+  end
+
+  def update
+    movie = Movie.find(params[:id])
+
+    if movie.update(movie_params)
+      render json: {status: 200, movie: movie}
+    else
+      render json: {status: 422}
+    end
+  end
+
+  def destroy
+    movie = Movie.find(params[:id])
+
+    movie.destroy
+  end
+
+  private
+    def movie_params
+      params.required(:movie).permit(:title, :poster)
+    end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  validates :email, presence: true, uniqueness: true
+  has_secure_password
+
+  has_many :reviews
+  has_many :movies, through: :reviews
+end
