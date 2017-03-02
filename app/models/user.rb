b427c92de983e76bc72bc5af10d362dbc8f09d46
currymich <currymich@gmail.com>
2017-03-02 13:02:39 -0800

    requires unique username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  validates :email, presence: true, uniqueness: true
+  validates :email, :username, presence: true, uniqueness: true
   has_secure_password
 
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   has_many :movies, through: :reviews
 end
