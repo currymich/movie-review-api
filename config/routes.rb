01c0f7c11cc509566019fe10addd276402c63e0c
currymich <currymich@gmail.com>
2017-03-02 13:04:41 -0800

    modified routes to hit new functions

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,33 @@
 Rails.application.routes.draw do
-  resources :movies
-  resources :users, only: [:create, :show, :destroy] do
+  resources :movies, only: [:index, :show, :create] do
+    collection do
+      get '/:id/reviews', to: 'movies#reviews'
+    end
+  end
+  resources :reviews
+  resources :users, only: [:create, :show] do
     collection do
       post '/login', to: 'users#login'
+      get '/:id/reviews', to: 'users#reviews'
     end
-    resources :reviews
   end
-  get '/get_all', to: 'reviews#get_all'
 end
+
+
+# Prefix Verb        URI Pattern                          Controller#Action
+#        GET         /movies/:id/reviews(.:format)        movies#reviews
+# movies GET         /movies(.:format)                    movies#index
+#        POST        /movies(.:format)                    movies#create
+#  movie GET         /movies/:id(.:format)                movies#show
+
+# reviews GET        /reviews(.:format)                   reviews#index
+#        POST        /reviews(.:format)                   reviews#create
+# review GET         /reviews/:id(.:format)               reviews#show
+#        PATCH       /reviews/:id(.:format)               reviews#update
+#        PUT         /reviews/:id(.:format)               reviews#update
+#        DELETE      /reviews/:id(.:format)               reviews#destroy
+
+# login  POST        /users/login(.:format)               users#login
+#        GET         /users/:id/reviews(.:format)         users#reviews
+#  users POST        /users(.:format)                     users#create
+#   user GET         /users/:id(.:format)                 users#show
